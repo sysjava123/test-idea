24cbbf3e46837f5fb51177987a6f94badeea5bc6
sys <1425546057@qq.com>
2022-09-04 19:27:30 +0800

    第一次新建项目

---
+++ test_idea1/src/com/doyens/Main.java
@@ -0,0 +1,14 @@
+package com.doyens;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("llllllllllllllllll");
+        System.out.println("llllllllllllllllll");
+        System.out.println("llllllllllllllllll");
+        System.out.println("llllllllllllllllll");
+        System.out.println("llllllllllllllllll");
+
+
+    }
+}
